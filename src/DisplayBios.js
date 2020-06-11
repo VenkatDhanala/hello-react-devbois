90394c59769f1e66af56c083bbbd1a0d05efe3bb
M_688776 <jason.monroe@cognizant.com>
2020-06-10 20:30:13 -0500

    partially completed forms

--- src/DisplayBios.js
+++ src/DisplayBios.js
@@ -1,24 +1,11 @@
-import React, { Component } from 'react'
-import Developer from './Developer';
 import DeveloperBio from './DeveloperBio';
 
-export class DisplayBios extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            developers:[
-                new Developer(1,"Jason","Monroe","JavaScript",2006),
-                new Developer(2,"Bill","Gates","BASIC",1970) 
-            ]
-        }
-    }
-    render() {
-        return (
-            this.state.developers.map(
-                (dev) => <DeveloperBio key={dev.id} developer={dev} />
-            )
-        );
-    }
+function DisplayBios(props){
+    return (
+        props.developers.map(
+            (dev) => <DeveloperBio key={dev.id} developer={dev} />
+        )
+    );
 }
 
 export default DisplayBios;
