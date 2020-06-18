141cefeca3212053da42ef3615994ef1fe2e0e63
M_688776 <jason.monroe@cognizant.com>
2020-06-18 18:51:45 -0500

    initial App.test passing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import configureStore from 'redux-mock-store';
+
+import App from '../components/App';
+import { unmountComponentAtNode } from 'react-dom';
+
+const initialState = {developer:[]};
+const mockStore = configureStore();
+let store, container;
+
+beforeEach(()=>{
+  store = mockStore(initialState);
+  container = document.createElement("div");
+})
+
+afterEach(()=>{
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App store={store}/>, container);
   const linkElement = getByText(/learn react/i);
+
   expect(linkElement).toBeInTheDocument();
 });
